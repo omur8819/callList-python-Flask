2b102e35958546ee0fec0ab269f3bc251c25b3d3
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-12-27 22:28:55 +0300

    error.html added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 import requests, json
 
 app = Flask(__name__)
@@ -9,22 +9,22 @@ def index():
 
 @app.route("/api", methods = ["GET", "POST"])
 def api():
-    if request.method == "POST":
+    
 
-     # get dates from index html
-        startdate = request.form.get("startDate").replace(" ", "%")
-        finishdate = request.form.get("finishDate").replace(" ", "%")
-
-        # # get json data from api
-        url = "https://staging1.alo-tech.com/api/?function=reportsCDRLogs&startdate={}&finishdate={}"
-        end_point = url.format(startdate, finishdate)
-        app_token = "ag9zfnRlbGVmb25pLXRlc3RyHwsSElRlbmFudEFwcGxpY2F0aW9ucxiAgICw46OcCQyiARVzdGFnaW5nMS5hbG8tdGVjaC5jb20"
-        data = requests.get(end_point, params={"app_token": app_token})
+    # get dates from index html
+    startdate = request.form.get("startDate").replace(" ", "%")
+    finishdate = request.form.get("finishDate").replace(" ", "%")
+    
+    # # get json data from api
+    url = "https://staging1.alo-tech.com/api/?function=reportsCDRLogs&startdate={}&finishdate={}"
+    end_point = url.format(startdate, finishdate)
+    app_token = "ag9zfnRlbGVmb25pLXRlc3RyHwsSElRlbmFudEFwcGxpY2F0aW9ucxiAgICw46OcCQyiARVzdGFnaW5nMS5hbG8tdGVjaC5jb20"
+    data = requests.get(end_point, params={"app_token": app_token})
+    if request.method == "POST" and data.status_code == 200 :
         json_data = data.json()["CallList"]
-
         return render_template("calltable.html", callData =json_data )
     else:
-        return render_template("calltable.html")
+        return render_template("error.html", error = data.status_code)
 
 @app.route("/calltable")
 def callTable():
